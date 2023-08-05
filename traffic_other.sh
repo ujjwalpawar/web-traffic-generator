437a10e5665c64999d02b76c25ac353cd962bae7
Ujjwal Pawar <s2325287@ed.ac.uk>
2023-08-05 14:18:37 +0100

    Update traffic_other.sh

--- traffic_other.sh
+++ traffic_other.sh
@@ -4,8 +4,8 @@ timeout 600 mpv https://www.dailymotion.com/video/x7eklbf --vo=null -v > logs_vi
 
 sleep 30
 
-timeout 600 wget -o- https://speed.hetzner.de/1GB.bin > logs_wget
+timeout 600 wget -o- https://speed.hetzner.de/1GB.bin | tee install.log
 
 sleep 30
 
-timeout 600 python gen.py > logs_web
+timeout 600 python gen.py > logs_web | tee install.log
